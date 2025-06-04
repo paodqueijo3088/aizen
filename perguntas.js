957d4c7bb10452268179ce7d89c8e98d62044063
paodqueijo3088 <maycon.gama.alves@escola.pr.gov.br>
2025-06-04 07:34:20 -0300

    bn

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'carro',
+    'o que é um motor V8',
+    'Um motor V8 é um tipo de motor de combustão interna com oito cilindros dispostos em duas bancadas de quatro cilindros cada, formando um "V" quando vistos de frente. Esses motores são conhecidos por produzirem alta potência e torque, o que os torna populares em carros esportivos, de luxo, SUVs grandes e picapes.'
 )
 
 criaCartao(
-    'Geografia',
+    'carro',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'carro',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'carro',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
